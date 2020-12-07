04497bc1d3ad3dd29a359a82dbfd660588c5d39a
Emma Norgren <emmanorgren85@gmail.com>
2020-12-07 06:54:27 +0100

    Day 7

--- python/07.py
+++ python/07.py
@@ -1,18 +1,9 @@
-import re
-
-
-with open('../input/07.txt') as stream:
-    lines = [line.strip() for line in stream]
-
-
-
 def parse_rules(lines):
     contains = {}
     contained_in = {}
     for line in lines:
         parts = line.split()
         bag = f'{parts[0]} {parts[1]}'
-        #print(bag)
         tmp = {}
         for i in range(4, len(parts), 4):
             if parts[i] != 'no':
@@ -22,8 +13,6 @@ def parse_rules(lines):
                 x.add(bag)
                 contained_in[bag2] = x
         contains[bag] = tmp
-    #print(contains)
-    #print(contained_in)
     return contains, contained_in
 
 def part1(bag, contained_in):
@@ -34,7 +23,6 @@ def part1(bag, contained_in):
             _find(parent)
     _find(bag)
     print(len(options))
-    #return options
 
 def part2(bag, contains):
     def _count(bag):
@@ -44,23 +32,10 @@ def part2(bag, contains):
             n += count * _count(name)
         return n
     print(_count(bag))
-            #if x != 0:
-            #    n += x * count
-            #print(name, count)
-            #n += (count * _find(name))
-            #_find(name)
-    #if bag in contains:
-    #return _find(bag)
-    #return n
 
+with open('../input/07.txt') as stream:
+    lines = [line.strip() for line in stream]
 
 contains, contained_in = parse_rules(lines)
 part1('shiny gold', contained_in)
 part2('shiny gold', contains)
-#print(len(find_options('shiny gold', contained_in)))
-
-
-    #parts = line.split()
-    #print(parts[0], parts[1])
-    #print(line.split())
-
